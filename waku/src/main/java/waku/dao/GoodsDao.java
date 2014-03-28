b279644109d47ee9e2861e6e6451958267eab985
bit <bit@localhost>
2014-03-28 15:47:13 +0900

    .

--- waku/src/main/java/waku/dao/GoodsDao.java
+++ waku/src/main/java/waku/dao/GoodsDao.java
@@ -7,6 +7,7 @@ import waku.vo.Goods;
 
 public interface GoodsDao {
 	public List<Goods> goodsInformation(Map<String, Object> goodsMapNumber) throws Exception;
+	public List<Goods> soldOutGno (int gNo) throws Exception;
 	public List<Goods> ByCompany(int cNo) throws Exception;
 	public List<Goods> delete(int gNo) throws Exception;
 	public List<Goods> selectList() throws Exception;
